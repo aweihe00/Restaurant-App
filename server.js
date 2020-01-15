447bf00c9d66c6ff92938264f51a4d654d43884b
Alyssa <lemryaly@gmail.com>
2020-01-14 20:03:44 -0600

    changes to block of code/ fixed errors/ syntax errors with code

--- server.js
+++ server.js
@@ -24,42 +24,49 @@ const Waitlist = [
     }
 ]
 
-let currentResNumber= 1; 
-//May need a different route here? 
-// app.post("/api/reserve", function(req, res) {
-//     const newReservation = req.body; 
-// ​
-//     newReservation.tableNumber =  currentResNumber; 
-   
-//     if (currentResNumber <= 5){
-//         Reservations.push(newReservation); 
-//     } else {
-//         Waitlist.push(newReservation); 
-//     }
-
-//     currentResNumber++; 
-// ​
-//     //Need to decide what we are doing after the post
-//     res.json(); 
-// }); 
-
-
-
-
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html" ));
 });
 
 app.get("/table", function(req, res) {
-    res.sendFile(path.join(__dirname, "table.html" ))
+    res.sendFile(path.join(__dirname, "tables.html" ))
 });
  app.get("/reserve", function(req, res) {
      res.sendFile(path.join(__dirname, "reserve.html"))
  });
 app.get("/api/Reservations", function(req, res) {
-    return res.join(characters)
+    return res.join(Reservations);
+});
+app.get("api/waitlist/", function(req, res) {
+    return res.join(Waitlist);
 });
 
 app.listen(PORT, function(){
     console.log("app listening on " + PORT);
 });
+
+let currentResNumber= 1; 
+app.post("/reserve", function(req, res) {
+​
+    var newReservation = req.body;
+    newReservation.tableNumber =  currentResNumber; 
+    
+    if (currentResNumber <= 5){
+        Reservations.push(newReservation); 
+    } else {
+        Waitlist.push(newReservation); 
+    }
+​
+    currentResNumber += 1;  
+  
+    console.log("The new Reservation is ")
+    console.log(newReservation);
+    console.log("The list of Reservations is ")
+    console.log(Reservations); 
+    console.log("This is the waitlist")
+    console.log(Waitlist); 
+  
+  
+    res.json(newReservation);
+  });
+
